a48358cc66e11e06bbd13591be0daad71faf407f
Sergey Kiyan <sergey198828@mail.ru>
2015-09-20 16:16:17 +0300

    Heap sort implemented

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args){
         int[] sourceArray = {2,3,4,1,8,5};
         MyArray array1 = new MyArray(sourceArray);
-        array1.mergeSort();
+        array1.heapSort();
         System.out.println(array1);
     }
 }
--- src/MyArray.java
+++ src/MyArray.java
@@ -7,6 +7,7 @@ public class MyArray {
     private int length;
     private int[] values;
 //Methods
+  //Constructors
     //Constructor using standard array initialization
     public MyArray(int[] values) {
         this.length = 0;
@@ -21,12 +22,21 @@ public class MyArray {
         this.length = array.length;
         this.values = array.values;
     }
+  //Generic methods
     //Length getter
     public int getLength() {
         return length;
     }
+    //To String method
+    @Override
+    public String toString() {
+        return "MyArray{" +
+                "values=" + Arrays.toString(values) +
+                '}';
+    }
+  //Merge sort methods
     //Merge function, both arrays MUST be sorted before run this function otherwise it wont work correctly
-    public MyArray merge(MyArray array){
+    private MyArray merge(MyArray array){
         int resultArrayLength = this.length+array.length; //identify future result array lenght
         int[] resultArray = new int[resultArrayLength]; //Allocate place for future result array
         int i=0; //First array counter
@@ -81,11 +91,42 @@ public class MyArray {
         //Updating current object
         this.values=q.poll().values;
     }
-
-    @Override
-    public String toString() {
-        return "MyArray{" +
-                "values=" + Arrays.toString(values) +
-                '}';
+  //Heap sort methods
+    //Make sub array [nodeIndex;size] heap
+    private void heapify(int nodeIndex, int size) {
+        nodeIndex++;
+        int maxIndex = nodeIndex; //Container for index of node with maximum value
+        int left = 2 * nodeIndex; //Index of left child
+        int right = left + 1; //Index of right child
+        // If left's child value is greater than node value then change index of node with maximum value to left node index
+        if (left <= size && this.values[maxIndex-1] < this.values[left-1])
+            maxIndex = left;
+        //  If right's child value is greater than node value change index of node with maximum value to right's node index
+        if (right <= size && this.values[maxIndex-1] < this.values[right-1])
+            maxIndex = right;
+        //If node value not maximum then swap and call recursively
+        if (maxIndex != nodeIndex) {
+            int temp = this.values[nodeIndex-1];
+            this.values[nodeIndex-1] = this.values[maxIndex-1];
+            this.values[maxIndex-1] = temp;
+            this.heapify(maxIndex-1, size);
+        }
+    }
+    //Make whole array heap
+    private void buildHeap(){
+        for(int i=this.length/2+1;i>=0;i--) {
+            this.heapify(i,this.length);
+        }
+    }
+    //Sorting using heap algorithm
+    public void heapSort(){
+        this.buildHeap();
+        int size = this.length-1;
+        while(size>0){
+            int temp = this.values[0];
+            this.values[0] = this.values[size];
+            this.values[size] = temp;
+            this.heapify(0, size--);
+        }
     }
 }
